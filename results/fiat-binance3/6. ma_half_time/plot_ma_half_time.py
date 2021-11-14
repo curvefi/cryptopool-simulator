1cc9d1c7f49662f1f3d94e81b65a6b434e92d50b
Michael Egorov <michwill@gmail.com>
2021-11-14 17:21:05 +0100

    Scan for optimal half_time

---
+++ results/fiat-binance3/6. ma_half_time/plot_ma_half_time.py
@@ -0,0 +1,28 @@
+import json
+import sys
+import pylab
+
+if len(sys.argv) > 1:
+    fname = sys.argv[1]
+else:
+    fname = 'results.json'
+
+with open(fname) as f:
+    results = json.load(f)
+
+gammas = []
+Z1 = []
+Z2 = []
+Z3 = []
+Z4 = []
+
+for row in results['configuration']:
+    # APY
+    # liq_density
+    gammas.append(row['ma_half_time'])
+    # Z1.append(row['Result']['liq_density'])
+    Z2.append(row['Result']['volume'])
+
+# pylab.semilogx(gammas, Z1)
+pylab.semilogx(gammas, Z2)
+pylab.show()
