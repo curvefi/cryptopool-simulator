f4651c3d0c2c28e860567c5d094097746d66258a
Michael Egorov <michwill@gmail.com>
2023-09-03 19:16:45 +0200

    Results 1

---
+++ tricrypto-resim/2. gamma-fee_gamma, fee 0.03-0.5/make_inputs_A_gamma.py
@@ -0,0 +1,43 @@
+import numpy as np
+from math import log10
+import json
+import itertools
+from copy import copy
+
+
+X = np.logspace(log10(1e-7), log10(2e-3), 32)
+Xname = "fee_gamma"
+Y = np.logspace(log10(1e-6), log10(3e-5), 32)
+Yname = "gamma"
+
+other_params = dict(
+    D=50e6,
+    adjustment_step=1e-7,
+    fee_gamma=0.006,
+    ma_half_time=600,
+    mid_fee=0.0003,
+    out_fee=0.008,
+    gas_fee=10,
+    n=3,
+    log=0,
+    allowed_extra_profit=1e-10,
+    ext_fee=0.0005,
+    gamma=1e-5,
+    A=10)
+
+config = {
+    'configuration': [],
+    'datafile': [
+        "ng-btcusdt-1m",
+        "ng-ethusdt-1m",
+        "ng-ethbtc-1m"],
+    'debug': 0}
+
+for x, y in itertools.product(X, Y):
+    params = copy(other_params)
+    params[Xname] = x
+    params[Yname] = y
+    config['configuration'].append(params)
+
+with open('configuration.json', 'w') as f:
+    json.dump(config, f)
