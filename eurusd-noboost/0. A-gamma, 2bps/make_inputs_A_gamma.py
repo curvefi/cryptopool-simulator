0618e5830db51e092d50f2eb7dfd3074c3fd0390
Michael Egorov <michwill@gmail.com>
2024-03-28 18:10:52 +1100

    Resumulating EUR with much denser params

---
+++ eurusd-noboost/0. A-gamma, 2bps/make_inputs_A_gamma.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+import numpy as np
+from math import log10
+import json
+import itertools
+from copy import copy
+
+
+X = np.logspace(log10(1), log10(50), 32)
+Xname = "A"
+Y = np.logspace(log10(1e-2), log10(0.35), 32)
+Yname = "gamma"
+
+other_params = dict(
+    D=40e3,
+    adjustment_step=1e-7,
+    fee_gamma=2e-3,
+    ma_half_time=600,
+    mid_fee=0.0002,
+    out_fee=0.003,
+    gas_fee=0,
+    n=2,
+    log=0,
+    allowed_extra_profit=1e-12,
+    ext_fee=0.0002,
+    gamma=1e-5,
+    boost_rate=0.0,
+    A=200)
+
+config = {
+    'configuration': [],
+    'datafile': ["xignite-eurusd-1m"],
+    'debug': 0}
+
+for x, y in itertools.product(X, Y):
+    params = copy(other_params)
+    params[Xname] = x
+    params[Yname] = y
+    config['configuration'].append(params)
+
+with open('configuration.json', 'w') as f:
+    json.dump(config, f)
