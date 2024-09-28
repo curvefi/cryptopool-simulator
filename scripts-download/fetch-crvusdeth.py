fcb4257c6721386936431e7fbd2b5a5fd875a44d
Michael Egorov <michwill@gmail.com>
2024-09-28 12:09:21 +0200

    TriCRV data for simulations (from Aug 1 2023)

--- scripts-download/fetch-crvusdeth.py
+++ scripts-download/fetch-crvusdeth.py
@@ -5,22 +5,40 @@ import time
 import requests
 import json
 
-pairs = ['CRVETH', 'CRVUSDT', 'ETHUSDT']
+POOL = "0x4eBdF703948ddCEA3B11f675B4D1Fba9d2414A14"
+crvUSD = "0xf939E0A03FB07F59A73314E73794Be0E57ac1b4E"
+ETH = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"
+CRV = "0xD533a949740bb3306d119CC777fa900bA034cd52"
 
-URI_TEMPLATE = 'https://api.binance.com/api/v1/klines?symbol=%s&interval=1m&limit=500&startTime={start}&endTime={end}'
-data = []
+pairs = {
+    'ETHUSD': (crvUSD, ETH),
+    'CRVUSD': (crvUSD, CRV),
+    'CRVETH': (ETH, CRV)
+}
 
-begin = datetime.datetime(year=2022, month=2, day=1)
+CHUNKS_IN_DAY = 5
+URI_TEMPLATE = 'https://prices.curve.fi/v1/ohlc/ethereum/%s?main_token={base_token}&reference_token={trade_token}&agg_number=1&agg_units=minute&start={start}&end={end}' % POOL
+
+s = requests.session()
+begin = datetime.datetime(year=2023, month=8, day=1)
 start = datetime.datetime.utcnow()
 dt = (start - begin)
-for pair in pairs:
-    for i in range(-dt.days * 5, 0):
-        d = int((start + datetime.timedelta(days=1) * i / 5).timestamp()) * 1000
-        uri = (URI_TEMPLATE % pair).format(start=d, end=d + 86400 * 1000 // 5 - 1)
-        resp = requests.get(uri).json()
-        print(pair, datetime.datetime.fromtimestamp(d // 1000), len(resp))
-        data += resp
-        time.sleep(0.1)
+
+for pair, (base_token, trade_token) in pairs.items():
+    data = []
+    for i in range(-dt.days * CHUNKS_IN_DAY, 0):
+        d = int((start + datetime.timedelta(days=1) * i / CHUNKS_IN_DAY).timestamp())
+        uri = URI_TEMPLATE.format(start=d, end=d + 86400 // CHUNKS_IN_DAY - 1, base_token=base_token, trade_token=trade_token)
+        while True:
+            try:
+                time.sleep(0.1)
+                resp = s.get(uri).json()
+                break
+            except Exception as e:
+                print(e)
+                time.sleep(30)
+        print(pair, datetime.datetime.fromtimestamp(d), len(resp['data']))
+        data += [[r['time'] * 1000, r['open'], r['high'], r['low'], r['close'], 1000000] for r in resp['data']]
 
     with open(pair.lower() + '.json', 'w') as f:
         json.dump(data, f)
