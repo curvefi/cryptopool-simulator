b88bdc494f50df890a0e18973ae1a5b674ac819b
Michael Egorov <michwill@gmail.com>
2024-10-20 00:16:06 +0200

    Simulate with stableswap instead of cryptoswap in cryptoswap

---
+++ btcusd-boost/stableswap/0. A/plot_apy.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import json
+import sys
+import pylab
+
+X = "A"
+
+if len(sys.argv) > 1:
+    fname = sys.argv[1]
+else:
+    fname = 'results.json'
+
+with open(fname) as f:
+    results = json.load(f)
+
+x = []
+Z = []
+
+for row in results['configuration']:
+    x.append(row[X])
+    Z.append(row['Result']['APY'])
+
+pylab.semilogx(x, Z)
+pylab.show()
---
+++ btcusd-boost/stableswap/1. A-mid_fee/make_inputs.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+import numpy as np
+from math import log10
+import json
+import itertools
+from copy import copy
+
+
+X = np.logspace(log10(1), log10(20), 32)
+Xname = "A"
+Y = np.logspace(log10(5e-4), log10(0.1), 32)
+Yname = "mid_fee"
+
+other_params = dict(
+    D=40e6,
+    adjustment_step=1e-7,
+    fee_gamma=0.003,
+    ma_half_time=600,
+    mid_fee=0.003,
+    out_fee=0.003,
+    gas_fee=5,
+    n=2,
+    log=0,
+    allowed_extra_profit=1e-10,
+    ext_fee=0.0003,
+    gamma=0,
+    boost_rate=0.05,
+    A=10)
+
+config = {
+    'configuration': [],
+    'datafile': ["btcusdt-2023-2024"],
+    'debug': 0}
+
+for x, y in itertools.product(X, Y):
+    params = copy(other_params)
+    params[Xname] = x
+    params[Yname] = y
+    params['out_fee'] = y
+    config['configuration'].append(params)
+
+with open('configuration.json', 'w') as f:
+    json.dump(config, f)
