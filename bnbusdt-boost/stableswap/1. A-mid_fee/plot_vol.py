a4b77ff816aba6856dc533836dae78a9e6383f4e
Michael Egorov <michwill@gmail.com>
2024-12-12 23:22:07 +0100

    BNB calculations

---
+++ bnbusdt-boost/stableswap/1. A-mid_fee/plot_vol.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+import json
+import sys
+import numpy as np
+# from math import log
+import matplotlib.pyplot as plt
+
+if len(sys.argv) > 1:
+    fname = sys.argv[1]
+else:
+    fname = 'results.json'
+
+with open(fname) as f:
+    results = json.load(f)
+
+As = set()
+gammas = set()
+
+x_axis = 'A'
+y_axis = 'mid_fee'
+
+for row in results['configuration']:
+    As.add(row[x_axis])
+    gammas.add(row[y_axis])
+
+As = sorted(list(As))
+gammas = sorted(list(gammas))
+
+Z = np.zeros((len(gammas), len(As)))
+
+for row in results['configuration']:
+    # APY
+    # liq_density
+    # volume
+    Z[gammas.index(row[y_axis]), As.index(row[x_axis])] = row['Result']['volume']
+
+fig, ax = plt.subplots()
+plt.yscale('log')
+plt.xscale('log')
+im = ax.pcolormesh(As, gammas, Z, cmap=plt.get_cmap('jet'))
+fig.colorbar(im, ax=ax)
+
+plt.show()
---
+++ bnbusdt-boost/stableswap/A1.5/2. out_fee-fee_gamma, mid_fee 0.3/make_inputs.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+import numpy as np
+from math import log10
+import json
+import itertools
+from copy import copy
+
+
+X = np.logspace(log10(0.003), log10(0.1), 64)
+Xname = "out_fee"
+Y = np.logspace(log10(1e-5), log10(2), 64)
+Yname = "fee_gamma"
+
+other_params = dict(
+    D=40e6,
+    adjustment_step=1e-7,
+    fee_gamma=0.003,
+    ma_half_time=600,
+    mid_fee=0.003,
+    out_fee=0.01,
+    gas_fee=5,
+    n=2,
+    log=0,
+    allowed_extra_profit=1e-10,
+    ext_fee=0.0003,
+    gamma=0,
+    boost_rate=0.05,
+    A=1.5)
+
+config = {
+    'configuration': [],
+    'datafile': ["bnbusdt-2021-2024"],
+    'debug': 0}
+
+for x, y in itertools.product(X, Y):
+    params = copy(other_params)
+    params[Xname] = x
+    params[Yname] = y
+    config['configuration'].append(params)
+
+with open('configuration.json', 'w') as f:
+    json.dump(config, f)
